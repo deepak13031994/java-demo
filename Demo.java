45d0d0f684b02a0a0b2e964a40e753b2155847bb
DEEPAK KUMAR SAH <deepak.bhumca2015@gmail.com>
2025-09-07 09:44:11 +0530

    add 2 numbers

--- Demo.java
+++ Demo.java
@@ -1,6 +1,13 @@
+import java.util.Scanner;
+
 public class Demo {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("Welcome to the Java Demo!");
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println("Sum: " + (a + b));
+        sc.close();
     }
 }
